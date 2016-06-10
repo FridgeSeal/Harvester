23106172fed5cdfe6f0111fec1c42076e11f48ce
FridgeSeal <tlw795@uowmail.edu.au>
2016-06-10 16:36:21 +1000

    Initial version of script to filter dataframe and match ID's.

--- ID_Matching.R
+++ ID_Matching.R
@@ -1,1 +1,13 @@
+options(stringsAsFactors = FALSE)
+library(data.table) # Because fread is ludicrous fast for big files
+library(dplyr)
 
+id_Data = fread(input = 'ID_Pairs.csv', header = TRUE, sep = ',')
+target_Pairs = fread(input = 'target_pairs.csv', header = TRUE, sep = ',')
+colnames(id_Data) = c('track.IDFA', 'OpID', 'AAID', 'IDFA', 'track.AAID')
+id_Data = id_Data[!((id_Data[['AAID']] == 'null') & (id_Data[['IDFA']] == 'null')) & (((id_Data[['IDFA']] != 'null') & ((id_Data[['track.IDFA']] == 'YES') | (id_Data[['track.IDFA']] == 'yes'))) | ((id_Data[['AAID']] != 'null') & ((id_Data[['track.AAID']] == 'null') | (id_Data[['track.AAID']] == 0))))] # It's a single monster line of code
+# But in one line it filters out: opID's from Mweb (both idfa and aaid missing)
+# idfa's and aaid's with tracking turned off or opted out. :)
+matched_pairs = as.data.frame(sapply(target_Pairs, match, id_Data[['OpID']]))
+colnames(matched_pairs) = c('row_num') 
+matched_pairs = id_Data[c(matched_pairs[['row_num']])]
