e398a1f1db891db3ac835321dac7ad4b10470924
FridgeSeal <tlw795@uowmail.edu.au>
2016-06-10 17:04:01 +1000

    Fixed a smal typo.

--- ID_Matching.R
+++ ID_Matching.R
@@ -3,7 +3,7 @@ library(data.table) # Because fread is ludicrous fast for big files
 library(dplyr)
 
 id_Data = fread(input = 'ID_Pairs.csv', header = TRUE, sep = ',')
-target_Pairs = fread(input = 'target_pairs.csv', header = TRUE, sep = ',')
+target_Pairs = fread(input = 'target_Pairs', header = TRUE, sep = ',')
 colnames(id_Data) = c('track.IDFA', 'OpID', 'AAID', 'IDFA', 'track.AAID')
 id_Data = id_Data[!((id_Data[['AAID']] == 'null') & (id_Data[['IDFA']] == 'null')) & (((id_Data[['IDFA']] != 'null') & ((id_Data[['track.IDFA']] == 'YES') | (id_Data[['track.IDFA']] == 'yes'))) | ((id_Data[['AAID']] != 'null') & ((id_Data[['track.AAID']] == 'null') | (id_Data[['track.AAID']] == 0))))] # It's a single monster line of code
 # But in one line it filters out: opID's from Mweb (both idfa and aaid missing)
